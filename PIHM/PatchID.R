952418b2289c59741ad8f0b212891c73d5f5cbda
Rajiv Mayani <mayani@isi.edu>
2019-01-11 15:11:05 -0500

    Code to identify cropland triangles and output their lat/long, elevation, etc

---
+++ PIHM/PatchID.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+
+"
+Code to read all triangles/cells/elements from Cell shape file.
+Tranform coodinates into latitude and longitudes and filter results for elements where land cover type as Cropland.
+"
+
+require('rgdal')
+require('rgeos')
+require('sp')
+require(zoo)
+require(xts)
+require(abind)
+require(ncdf4)
+library(raster)
+library(xts)
+
+# Land Cover: Cropland: 12
+ilc = 12
+
+x.pcs = readOGR('ModelInfo/GISlayer/Cells.shp');
+x.gcs = spTransform(x.pcs, CRSargs(CRS("+init=epsg:4326")))
+DF <- as.data.frame(x.gcs)
+
+ll = coordinates(x.gcs)
+# ll
+
+DF$X_c = ll[,1]
+DF$Y_c = ll[,2]
+
+DF = subset(DF, LC == ilc)
+
+write.csv(DF, file = "for-cycles.csv", row.names=F)
